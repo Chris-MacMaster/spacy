d762e77f7861a6b8a1c37bad8fd5e62f1bb4a749
Marc <meir.guggenheim@gmail.com>
2023-04-07 13:50:02 -0400

    product review

--- app/models/product_reviews.py
+++ app/models/product_reviews.py
@@ -1,18 +1,18 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
-class ProductImage(db.Model):
-    __tablename__ = 'product_images'
+class ProductReview(db.Model):
+    __tablename__ = 'product_reviews'
 
-    id = db.Column(db.Integer, primary_key = True)
-    url = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String, nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('product.id')))
 
-    product = db.relationship('Product', back_populates='product_images')
+    product = db.relationship('Product', back_populates='product_reviews')
 
     def to_dict(self):
         return {
             'id': self.id,
-            'url': self.url,
+            'text': self.text,
             'productId': self.product_id,
             'product': self.product
         }
