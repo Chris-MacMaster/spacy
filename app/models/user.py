6ee89de3dc94210c40e9caaa5c396a82d8b21316
Marc <meir.guggenheim@gmail.com>
2023-04-07 19:11:53 -0400

    deleted line

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app.models.follows import following_users
+# from app.models.follows import following_users
 from .follows import follows
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
