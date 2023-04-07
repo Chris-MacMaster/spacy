452f94149163731c28193ba5d05123914272486a
root <root@Endiness.localdomain>
2023-04-07 11:24:41 -0700

    added cart, review_images, shop_images models

---
+++ app/models/shop_images.py
@@ -0,0 +1,18 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class ShopImage(db.Model):
+    __tablename__ = 'shop_images'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(255), nullable=False)
+
+    shop_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('shop.id')))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'url': self.url
+            }
