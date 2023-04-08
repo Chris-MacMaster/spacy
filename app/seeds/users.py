779e8740442b6e72b4b9294bf7b493cc3cc8d5c8
root <root@Endiness.localdomain>
2023-04-07 17:02:41 -0700

    added user seed data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,11 +5,30 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo',
+        email='demo@aa.io',
+        password='password',
+        first_name='Demo',
+        last_name='User',
+        bio='The most interesting demo user ever'
+        )
+
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie',
+        email='marnie@aa.io',
+        password='password',
+        first_name='Marnie',
+        last_name='Whoever',
+        bio='A SAHM who is also a serial killer on the side.'
+        )
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie',
+        email='bobbie@aa.io',
+        password='password',
+        first_name='Bobbie',
+        last_name='Bobberson',
+        bio='Bobbie is amazin'
+        )
 
     db.session.add(demo)
     db.session.add(marnie)
@@ -28,5 +47,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
