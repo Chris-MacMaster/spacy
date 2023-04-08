d5691eb5b190ac6c2aade49a901a970a6ee2018b
cludwell <cludwell@gmail.com>
2023-04-07 19:26:20 -0700

    seeding

--- app/seeds/shops.py
+++ app/seeds/shops.py
@@ -5,17 +5,17 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_shops():
     shop1 = Shop(
-        name='Galactic Outfitters',
-        owner_id = 1,
-        street_address = '123 Starship Blvd',
-        city = 'New Terra',
-        state = 'Zeta Quadrant',
-        country = 'Galactic Federation',
-        description = 'We sell the latest and greatest space gear for explorers and adventurers alike.',
-        category = 'Space Equipment',
-        sales = 100,
-        policies = 'All sales are final for used or damaged equipment.'
-        )
+            name='Galactic Outfitters',
+            owner_id = 1,
+            street_address = '123 Starship Blvd',
+            city = 'New Terra',
+            state = 'Zeta Quadrant',
+            country = 'Galactic Federation',
+            description = 'We sell the latest and greatest space gear for explorers and adventurers alike.',
+            category = 'Space Equipment',
+            sales = 100,
+            policies = 'We accept all major credit cards. Gift cards are coming soon. All sales are final for used or damaged equipment. We require payment in full before we can ship your order. If you are unhappy with your order please reach out and contact us, we might be able to arrange an exchange or store credit.'
+            )
 
     shop2 = Shop(
             name='Intergalactic Cafe',
@@ -27,7 +27,7 @@ def seed_shops():
             description = 'We serve up the best coffee and snacks in the galaxy.',
             category = 'Cafe',
             sales = 50,
-            policies = 'Refunds available for orders cancelled within 24 hours.'
+            policies = 'We strive for 100 percent customer satisfaction. If you are unable with your order, please reach out to us with 14 days of receiving your roder to request a return or exchange. Items must be returned in mint condition with all packaging and tags.'
             )
 
     shop3 = Shop(
@@ -40,7 +40,7 @@ def seed_shops():
             description = 'We design and create one-of-a-kind space fashion for the stylish space traveler.',
             category = 'Fashion',
             sales = 200,
-            policies = 'Custom orders are final sale and cannot be returned or exchanged.'
+            policies = 'We love to work on customer orders. Please contact us and let us know your vision, maybe we could collaborate! Please note that customer orders take longer to produce and will ship later than standard policies.'
             )
 
     db.session.add_all([shop1, shop2, shop3])
