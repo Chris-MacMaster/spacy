172b0b34a942bedf4608a583fc71f8f383666efb
cludwell <cludwell@gmail.com>
2023-04-07 23:42:40 -0700

    product reviews

--- app/seeds/product_reviews.py
+++ app/seeds/product_reviews.py
@@ -26,43 +26,78 @@ def seed_product_reviews():
     review04 = ProductReview(
         # shop and product tied to user 1, carl sagan
         user_id = 5,
-        product_id = 1,
+        product_id = 3,
         review = "These coasters are out of this world! 😉😏 The planets look so realistic, and I love that each one is different. They're also really practical - they're easy to clean and they protect my table from water rings. They arrived quickly and in great condition. Highly recommend!",
         stars = 5
     )
     review05 = ProductReview(
         # shop and product tied to user 1, carl sagan
         user_id = 3,
-        product_id = 1,
+        product_id = 2,
         review = "While I wanted to love this product, it's more of a jetpack than a backpack. While my son loves it, the storage is limited and the other kids at school complain that he smells like jet fuel. Maybe with a little more storage capacity I could recommend it to a friend.",
         stars = 3
     )
     review06 = ProductReview(
         # shop and product tied to user 1, carl sagan
         user_id = 2,
-        product_id = 1,
+        product_id = 2,
         review = "Arrived late and missed the holidays, even though I placed my order well in a month in advance. Very dissappointed. I reached out the shop owner and they were willing to make an exception and take the item back as a return. While I am disappointed, I wouldn't rule out making another purchase here in the future becauase there really isn't anyone else like them",
         stars = 4
     )
     review07 = ProductReview(
         # shop and product tied to user 1, carl sagan
         user_id = 4,
-        product_id = 1,
+        product_id = 2,
         review = "I think the seller is well intentioned, but I don't really see the point in this jetpack like contraption if we don't know what sort of atmosphere other worlds will have. Maybe the atmosphere there is too thin, maybe the gravitational pull means that this thing doesn't have enough power to actually get off the ground. I took a chance, because I am such a big fan of The Mandalorian and want to style a jet pack like Din Djarin. I will let you all know how it goes once I test this thing in the field.",
         stars = 3
     )
     review08 = ProductReview(
         # shop and product tied to user 1, carl sagan
         user_id = 3,
-        product_id = 1,
+        product_id = 3,
         review = "I love these coasters! They're so unique and fun. Each one features a different planet, and the colors and details are really beautiful. They're also really good quality - they're thick and sturdy, and the cork backing keeps them in place on my table. I've gotten a lot of compliments on them already!",
         stars = 5
     )
     review09 = ProductReview(
         # shop and product tied to user 1, carl sagan
         user_id = 4,
-        product_id = 1,
+        product_id = 3,
         review = "These coasters are really cool! The planets are beautifully rendered and the colors are so vibrant. The only reason I'm giving them 4 stars instead of 5 is that they're a little on the small side. They still work well as coasters, but I wish they were a bit bigger.",
+        stars = 5
+    )
+    review10 = ProductReview(
+        # shop and product tied to user 1, carl sagan
+        user_id = 2,
+        product_id = 4,
+        review = "The Astronaut Figurine is a high-quality collectible that exceeded my expectations. It arrived in perfect condition and is even more impressive in person than in the photos. Highly recommended!",
+        stars = 5
+    )
+    review11 = ProductReview(
+        # shop and product tied to user 1, carl sagan
+        user_id = 3,
+        product_id = 4,
+        review = "",
+        stars = 5
+    )
+    review12 = ProductReview(
+        # shop and product tied to user 1, carl sagan
+        user_id = 4,
+        product_id = 4,
+        review = "",
+        stars = 5
+    )
+    review13 = ProductReview(
+        # shop and product tied to user 1, carl sagan
+        user_id = 5,
+        product_id = 4,
+        review = "",
+        stars = 5
+    )
+    review14 = ProductReview(
+        # shop and product tied to user 1, carl sagan
+        user_id = 6,
+        product_id = 4,
+        review = "",
         stars = 5
     )
 
