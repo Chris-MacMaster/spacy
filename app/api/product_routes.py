bf52c645266df9dd7c3e40b3f4f3030c0142de0d
cludwell <cludwell@gmail.com>
2023-04-08 23:30:16 -0700

    finally figured out lazy loading

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,15 +1,25 @@
 from flask import Blueprint, jsonify
-from app.models import db, Product, Shop, User
+from app.models import db, Product, Shop, ProductImage
 from flask_login import current_user, login_required
-
+import copy
 product_routes = Blueprint('/products', __name__)
 
 
 @product_routes.route('/')
 def get_all_products():
     """returns all products regardless of session"""
+    # get products
     products = Product.query.all()
-    return { 'Products': { product.id: product.to_dict() for product in products } }, 200
+    productcopy = copy.deepcopy(products)
+    # helper function to get associated images of each product
+    def get_images(id):
+        return ProductImage.query.filter(ProductImage.product_id == id).all()
+    answer = {  product.id: product.to_dict() for product in productcopy }
+    for product in answer.values():
+        product_images = get_images(product['id'])
+        product['ProductImages'] = [image.to_dict() for image in product_images]
+
+    return answer, 200
 
 @product_routes.route('/current')
 @login_required
