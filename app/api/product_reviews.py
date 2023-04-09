85260b8e6113f256fb554b1a77412b95a0a00436
cludwell <cludwell@gmail.com>
2023-04-09 12:06:09 -0700

    reviews

--- app/api/product_reviews.py
+++ app/api/product_reviews.py
@@ -1,9 +1,17 @@
 from flask import Blueprint
-from app.models import db, Product, ProductReview
+from app.models import db, Product, ProductReview, ReviewImage
 
 product_review_routes = Blueprint('/productReviews', __name__)
 
-# @product_review_routes.route('/')
-# def get_all_reviews():
-#     """returns all reviews, which if normalized is useful for state"""
-#     products =
+@product_review_routes.route('/')
+def get_all_reviews():
+    """returns all reviews, which if normalized is useful for state"""
+    reviews = ProductReview.query.all()
+    reviewcopy = copy.deepcopy(reviews)
+    def get_review_images(id):
+        return ReviewImage.query.filter(ProductReview.id == id).all()
+    payload = { review.id: review.to_dict() for review in reviews }
+    for review in payload.values():
+        review_image = get_review_images(review['id'])
+        review['ReviewImages'] = review_image.to_dict()
+    return { 'ProductReviews': payload }, 200
