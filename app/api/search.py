1e5691f8331ee71ac814e5066060f670d754f143
Marc <meir.guggenheim@gmail.com>
2023-04-09 16:29:49 -0400

    search feature placeholder

---
+++ app/api/search.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, jsonify, request
+from app.models import db, Product, Shop, User
+
+
+search = Blueprint('/search', __name__)
+
+@search.route('/search', methods=['POST'])
+def search():
+    """search database for results"""
+    parameters = request.data['parameters'] #placeholder variable name
+    products_results = Product.query().filter(Product.name.like("%{parameters}%")
+                                or Product.category == parameters
+                                or [Shop.name == parameters for Product.shops.name in Product.shops]).all()
+    
+    if products_results:
+        return jsonify(products_results), 200
+    return {'No results match those terms'}, 404
+    
+    
