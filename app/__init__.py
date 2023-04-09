00231e5c21626cc413f965ee50b30b92109e87bd
Marc <meir.guggenheim@gmail.com>
2023-04-09 17:01:47 -0400

    modified file name for shop routes

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.product_routes  import product_routes
-from .api.shops_routes import shop_routes
+from .api.shop_routes import shop_routes
 from .api.product_reviews import product_review_routes
 from .api.search_route import search_route
 from .seeds import seed_commands
