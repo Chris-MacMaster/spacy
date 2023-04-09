4519caf168a17d0bdfd4074c0444f99d63aa6cbf
cludwell <cludwell@gmail.com>
2023-04-09 11:55:39 -0700

    review route

--- app/api/product_reviews.py
+++ app/api/product_reviews.py
@@ -3,7 +3,7 @@ from app.models import db, Product, ProductReview
 
 product_review_routes = Blueprint('/productReviews', __name__)
 
-# @product_review_routes.route('/')
-# def get_all_reviews():
-#     """returns all reviews, which if normalized is useful for state"""
-#     products =
+@product_review_routes.route('/')
+def get_all_reviews():
+    """returns all reviews, which if normalized is useful for state"""
+    reviews = ProductReview.query.all()
