b77404a5bd41332e18aee6c50304cd137b62d37f
christopher <christopher.macmaster@gmail.com>
2023-04-08 21:06:17 -0700

    main page chrisM

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, redirect, render_template
 from app.models import db, Product, Shop, User
 from flask_login import current_user, login_required
 
@@ -9,7 +9,15 @@ product_routes = Blueprint('/products', __name__)
 def get_all_products():
     """returns all products regardless of session"""
     products = Product.query.all()
-    return { 'Products': { product.id: product.to_dict() for product in products } }, 200
+    # WORKING ORIGINAL CODE
+    # return { 'Products': { product.id: product.to_dict() for product in products } }, 200
+
+    # ATTEMPT TO MAKE COMPATIBLE WITH TEMPLATE
+    formatted_products = { 'Products': { product.id: product.to_dict() for product in products } }
+    return render_template("main_page.html", products = products)
+
+
+
 
 @product_routes.route('/current')
 @login_required
@@ -20,3 +28,34 @@ def current_users_products():
         products = Product.query.filter_by(user_id=current_user.id).all()
         return { 'Products': {product.id: product.to_dict() for product in products } }, 200
     return '<h1>Please create an account</h1>'
+
+
+# add other details to response
+@product_routes.route('/<int:product_id>')
+def get_one_product(product_id):
+    """returns one product with the specified id"""
+    product = Product.query.filter_by(id=product_id).first()
+    return product.to_dict(), 200
+    
+
+#DEMO TESTING POST ROUTE, NOT CONNECTED TO FORM/USER INPUT YET
+#running "post" for url in postman works, but not url entry yet
+@product_routes.route('/new', methods=["POST"]) 
+def make_new_product():
+    """makes a new product"""
+    product = Product(
+        available = 20,
+        category = 'Jewelry',
+        description = 'Test description',
+        free_shipping = True,
+        name = "Test Name",
+        price = 29.99,
+    )
+    db.session.add(product)
+    db.session.commit()
+    return redirect('/')
+
+
+
+#DEMO POST 
+# @product_routes.route('/')
