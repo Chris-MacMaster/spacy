df3cac0184e02ceefb05c32ae64c91dff08bc3bd
DylanJG01 <DylanJ.Godeck@gmail.com>
2023-04-11 08:40:39 -0700

    cart route adds quantity

--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_login import current_user, login_required
 from app.models import Cart, Product, User, ProductImage
 
-cart_routes = Blueprint('/cart/', __name__)
+cart_routes = Blueprint('/cart', __name__)
 
 @cart_routes.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 @login_required
@@ -10,7 +10,6 @@ def return_cart():
     """
     Query for all products associated with the user's cart and returns them in a list of dictionaries
     """
-
     the_cart = Cart.query.filter_by(user_id=current_user.id).all()
     #print("*#!*#!*#!*#!*", the_cart[0].to_dict())
     cart_items = Product.query\
@@ -26,6 +25,7 @@ def return_cart():
         #print("@!@!@!@!@!", item.carts[0].quantity)
         cart[item.id] = product
         cart[item.id]["productImages"] = []
+        cart[item.id]["quantity"] = item.carts[0].quantity
         if item.product_images:
             cart[item.id]["productImages"].append(item.product_images[0].url)
         else:
