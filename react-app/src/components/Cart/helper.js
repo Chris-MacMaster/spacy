66c4092ef3648aca01f74a2848a74373e84d33a9
DylanJG01 <DylanJ.Godeck@gmail.com>
2023-04-12 15:56:03 -0700

    working on carthelper

--- react-app/src/components/Cart/helper.js
+++ react-app/src/components/Cart/helper.js
@@ -15,15 +15,22 @@ RETURN [[ITEMS FROM SHOP BLAHBLAH],[ITEMS FROM SHOP BLOOPBLOP]]
 export const groupItemsByStore = (cart) =>{
 
     const ans = {}
-
-    for (const item of cart) {
-        if (!ans[item.shopId]){
-            console.log(item.shopId)
-            ans[item.shopId] = {"arr": [item]}
-            ans[item.shopId].name = item.shopName
-        } else ans[item.shopId].arr.push(item)
+    console.log(cart)
+    // for (const item of cart) {
+    //     if (!ans[item.shopId]){
+    //         console.log(item.shopId)
+    //         ans[item.shopId] = {"arr": [item]}
+    //         ans[item.shopId].name = item.shopName
+    //     } else ans[item.shopId].arr.push(item)
+    // }
+    for (const i in cart){
+        console.log(ans[cart[i].shopName])
+        if (!ans[cart[i].shopName]){
+            ans[cart[i].shopName] = [cart[i]]
+        } else {
+            ans[cart[i].shopName].push(cart[i])
+        }
     }
-
     console.log("!!!!!!!!", ans)
     return ans
 }
