355036f79981517e66c7984cd81f3fc9fdb1d4e2
Marc <meir.guggenheim@gmail.com>
2023-04-14 12:05:57 -0400

    filtered search backend route

--- app/api/search_route.py
+++ app/api/search_route.py
@@ -91,6 +91,9 @@ def search(parameters):
                 product['avgRating'] = round(review_sum / len(reviews), 1) if len(reviews) > 0 else 'New!'
             product['reviews'] = [review.to_dict() for review in reviews]
 
+            shop = get_shop(product['shopId'])
+            product['shop'] = shop.to_dict()
+
 
         full_list = list_1 + list_2         
         print('results backend', full_list) #to dict
@@ -104,29 +107,78 @@ def search(parameters):
             return {'result': 'No items found'}
 
 
-@search_route.route('/filtered-search/<category>')
-def filtered_search(category):
+@search_route.route('/filtered-search/<search_category>')
+def filtered_search(search_category):
+    search_replace = search_category.replace('%', ' ')
+
+    def get_images(id):
+        return ProductImage.query.filter(ProductImage.product_id == id).all()
+    def get_reviews(id):
+        return ProductReview.query.filter(ProductReview.product_id == id).all()
+    def get_shop(id):
+        return Shop.query.filter(Shop.id == id).one()
+
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print(search_replace)
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
     products = None
-    if category == 'Gifts for Her':
-            products = Product.query.filter(or_(Product.category.ilike('Jewelry') or
-                                                Product.category.ilike('Home Decor') or
-                                                Product.category.ilike('Wall Art'))).all()
-    elif category == 'Gifts for Him':
-            products = Product.query.filter(or_(Product.category.ilike('Apparel') or
-                                                Product.category.ilike('Foodstuffs'))).all()
+
+
+    if search_replace == 'Gifts for Her':
+            products = Product.query.filter(or_((Product.category =='Jewelry') |
+                                                (Product.category =='Home Decor') |
+                                                (Product.category =='Wall Art'))).all()
+    elif search_replace == 'Gifts for Him':
+            products = Product.query.filter(or_((Product.category == 'Apparel') |
+                                                (Product.category =='Foodstuffs'))).all()
             
-    elif category == 'Gifts for Kids':
-            products = Product.query.filter(or_(Product.category.ilike('Collectibles')) or
-                                                Product.category.ilike('Food') or
-                                                Product.category.ilike('Soft Drink')).all()
-    elif category == 'Gifts Under $30':
+    elif search_replace == 'Gifts for Kids':
+            products = Product.query.filter(or_((Product.category =='Collectibles') |
+                                                (Product.category == 'Food') |
+                                                (Product.category == 'Soft Drink'))).all()
+    elif search_replace == 'Gifts Under $30':
             products = Product.query.filter(Product.price < 30).all()
 
     else:
             products = Product.query.all()
 
-    print('filtered products', products)
-    return {'Hello!'}
+
+    prod_list = [product.to_dict() for product in products]
+
+
+    for prod in prod_list:
+        product_images = get_images(prod['id'])
+        prod['ProductImages'] = [image.to_dict() for image in product_images]
+            
+        review_sum = 0
+        reviews = get_reviews(prod['id'])
+        for review in reviews:
+                review_sum += review.stars
+                prod['avgRating'] = round(review_sum / len(reviews), 1) if len(reviews) > 0 else 'New!'
+                prod['reviews'] = [review.to_dict() for review in reviews]
+
+
+                shop = get_shop(prod['shopId'])
+                prod['shop'] = shop.to_dict()
+
+#     print('filtered products', [product.to_dict() for product in products])
+    return prod_list
         
 
     
