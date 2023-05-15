4174080a1c30efa397ea52f070bdfb1b92c45860
DylanJG01 <DylanJ.Godeck@gmail.com>
2023-05-15 10:47:56 -0700

    started order model

---
+++ app/models/orders.py
@@ -0,0 +1,20 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy.sql import func
+
+class Purchase(db.Model):
+    __tablename__ = 'purchases'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    carts = db.relationship()
+
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+
+        }
