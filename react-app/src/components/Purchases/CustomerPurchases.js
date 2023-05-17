97e2bad9016ca88a2346a2453eca1f4647fc8c28
DylanJG01 <DylanJ.Godeck@gmail.com>
2023-05-17 12:04:01 -0700

    More css

--- react-app/src/components/Purchases/CustomerPurchases.js
+++ react-app/src/components/Purchases/CustomerPurchases.js
@@ -7,9 +7,8 @@ import './Purchases.css'
 export default function UserPurchases(){
     const [user, purchases] = useSelector(state => [state.session.user, state.purchases.userPurchases])
     const dispatch = useDispatch()
-    const [] = useState()
     const [hasLoaded, setHasLoaded] = useState(false)
-
+    const [totalCost, setTotalCost] = useState(0)
     useEffect(async () => {
         await dispatch(fetchUserPuchases())
         return setHasLoaded(true)
@@ -24,13 +23,13 @@ export default function UserPurchases(){
         {purchases && Object.keys(sortedPurchases).map(el => (
             <div className="purchase-date-div">
                 <p className="purchase-date">{el.slice(0,25)}</p>
-                    {console.log(sortedPurchases[el])}
                 {Object.values(sortedPurchases[el]).map(item => (
                     <div className="purchase-shop-div">
                         <div className="purchase-shop-name"> {item[0].shopName}</div>
                         <div className="two">Product</div>
                         <div className="three">Qty </div>
                         <div className="four">Price</div>
+                        <div className="five">Total</div>
                         {/* <div className="item-map-div"> */}
                             {item.map(el => (
                                 // <div className="purchase-item-div">
@@ -40,7 +39,6 @@ export default function UserPurchases(){
                                 <div className="three">{el.quantity}</div>
                                 <div className="four">{el.price}</div>
                                 <div>{el.price * el.quantity}</div>
-                                {/* </div> */}
                                 </>
                             ))}
                         </div>
@@ -53,6 +51,13 @@ export default function UserPurchases(){
 }
 
 function groupItemsBySellDateThenStore(purchases){
+    //This function takes a normalized object contatining all the purchases and then
+    //bundles them together first by date, then by store.
+    ///It returns an object that looks like this
+    /*
+    {date1: { store1: [item1, item2], store2: [item3]}, date2: {store3: [item1]}}
+    or thereabouts.
+    */
     const groupedItems = {}
     if(!purchases) return null
     for (const key in purchases) {
