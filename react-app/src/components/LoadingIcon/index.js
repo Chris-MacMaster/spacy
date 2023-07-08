b73d6a3a7a53d2a9c07822d1f9fa673375b7ec20
cludwell <cludwell@gmail.com>
2023-07-07 21:56:42 -0700

    clean up

--- react-app/src/components/LoadingIcon/index.js
+++ react-app/src/components/LoadingIcon/index.js
@@ -1,10 +1,10 @@
 import './LoadingIcon.css'
-import Rocket from '../../images/rocket'
+import rocket from '../../images/rocket.gif'
 export default function LoadingIcon() {
 
     return (
         <div className='loading-container'>
-            <img src={Rocket} alt='' />
+            <img src={rocket} alt='' className='loading-icon'/>
         </div>
     )
 }
