332743cf3dbe8351d1a3ef0227bf1eaa7c004e8a
cludwell <cludwell@gmail.com>
2023-11-20 18:36:34 -0800

    search results refactored

--- react-app/src/components/SearchResults.jsx
+++ react-app/src/components/SearchResults.jsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getSearchResults } from "../../store/search";
+import { getSearchResults } from "../store/search";
 import { NavLink, useParams } from "react-router-dom";
-
-import "./SearchResults.css";
-import LoadingIcon from "../LoadingIcon";
+import LoadingIcon from "./LoadingIcon";
 
 function SearchResults() {
   const { parameters } = useParams();
@@ -23,7 +21,7 @@ function SearchResults() {
   if (!hasLoaded) return <LoadingIcon />;
 
   return (
-    <div>
+    <div className="mx-4">
       <div className=" min-h-screen max-w-screen-lg mx-auto">
         <div className=" rounded-full border-solid border-black border-2 w-fit p-2 text-sm my-4">
           <span className="font-bold">Esitmated Arrival Time: </span>
@@ -33,68 +31,62 @@ function SearchResults() {
           {Object.values(products).map((product, i) => (
             <NavLink to={`/products/${product.id}`} key={`linked${i}`}>
               <div className="" key={`prod-card-${i}`}>
-                <div className="search-result-image-div" key={`image-div-${i}`}>
                   <img
                     className=" object-cover h-32 w-40 rounded-lg"
                     src={product.ProductImages[0].url}
                     alt="not found"
                     key={`img${i}`}
                   />
-                </div>
 
-                <div className="search-product-text" key={`prod-text-${i}`}>
-                  <p className="search-product-name" key={`prod-name-${i}`}>
+                <div className="" key={`prod-text-${i}`}>
+                  <p className="" key={`prod-name-${i}`}>
                     {product.name}
                   </p>
-                  <p className="search-results-stars" key={`prod-stars${i}`}>
+                  <p className="" key={`prod-stars${i}`}>
                     {typeof product.avgRating === "number" ? (
                       Array(5)
                         .fill(1)
                         .map((s, i) =>
                           i < product.avgRating ? (
                             <i
-                              className="fa-solid fa-star search-results-stars-gold search-stars"
+                              className="fa-solid fa-star text-sm"
                               key={i}
                             ></i>
                           ) : (
                             <i
-                              className="fa-solid fa-star search-results-stars-black grey-search-stars"
+                              className="fa-solid fa-star text-sm"
                               key={i}
                             ></i>
                           )
                         )
                     ) : (
-                      <span className="search-new-product" key={`new${i}`}>
+                      <span className="text-sm" key={`new${i}`}>
                         New!{" "}
                         <i
-                          className="fa-solid fa-star search-results-stars-gold "
+                          className="fa-solid fa-star text-sm"
                           key={`icon${i}`}
                         />{" "}
                       </span>
                     )}{" "}
                     {product.sales}
                     <span
-                      className="search-star-seller"
+                      className="text-xs font-bold"
                       key={`star-seller${i}`}
                     >
                       <i
-                        className="fa-solid fa-certificate search-badge"
+                        className="fa-solid fa-certificate text-purple-700 text-sm"
                         key={`badge${i}`}
                       ></i>
                       Star Seller
                     </span>
                   </p>
 
-                  <p className="search-result-price" key={`price${i}`}>
+                  <p className="font-bold" key={`price${i}`}>
                     ${product.price}
                   </p>
-                  <p className="search-result-shop" key={`shop${i}`}>
-                    {product.shop.name}
-                  </p>
-                  <p className="search-more-like" key={`morelikethis${i}`}>
-                    More Like This{" "}
-                    <i
-                      className="fa-solid fa-arrow-right search-more-like"
+                  <p className=" text-sm" key={`shop${i}`}>
+                    {product.shop.name} <i
+                      className="fa-solid fa-arrow-right"
                       key={`arrow${i}`}
                     ></i>
                   </p>
