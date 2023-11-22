57721eb9c7a970aca326ffe47617bccc04b70eae
cludwell <cludwell@gmail.com>
2023-11-21 19:28:05 -0800

    prod img slider is refactored, but width of container and img needs work

--- react-app/src/components/ProductImageSlider.jsx
+++ react-app/src/components/ProductImageSlider.jsx
@@ -22,16 +22,32 @@ export default function ProductImageSlider({
   if (!data || !data.length) return null;
   return (
     <div className="relative w-[50vmin] h-[40vmin] mb-[5vmin]">
-      <div className=" text-[4vmin] text-white opacity-80 absolute top-[20vmin] z-[1] rounded-full transition ease-in-out duration-300 left-[1vmin] " onClick={prevSlide}>
+      <div
+        className=" text-[4vmin] text-white opacity-80 absolute top-[20vmin] z-[1] rounded-full transition ease-in-out duration-300 left-[1vmin] "
+        onClick={prevSlide}
+      >
         <i className="fa-solid fa-circle-left " />
       </div>
-      <div className=" text-[4vmin] text-white opacity-80 absolute top-[20vmin] z-[1] rounded-full transition ease-in-out duration-300 right-[1vmin]" onClick={nextSlide}>
+      <div
+        className=" text-[4vmin] text-white opacity-80 absolute top-[20vmin] z-[1] rounded-full transition ease-in-out duration-900 right-[1vmin]"
+        onClick={nextSlide}
+      >
         <i className="fa-solid fa-circle-right" />
       </div>
       {data.map((img, i) => (
-        <div className={`transition ease-in-out duration-400 ${i === current ? "opacity-100 scale-100" : "opacity-0 scale-90"}`} key={i}>
+        <div
+          className={`transition ease-in-out duration-300 ${
+            i === current ? "opacity-100 scale-100" : "opacity-0 scale-90"
+          }`}
+          key={i}
+        >
           {i === current && (
-            <img src={img.url} className="w-[50vmin] h-[40vmin] rounded-xl object-cover" alt="" key={i} />
+            <img
+              src={img.url}
+              className="w-[50vmin] h-[40vmin] rounded-xl object-cover"
+              alt=""
+              key={i}
+            />
           )}
         </div>
       ))}
